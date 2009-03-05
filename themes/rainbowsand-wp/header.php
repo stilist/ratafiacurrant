97f453d03ea2f68ff56d8e10ed73b15b6b37c3ea
stilist <stilist+git@gmail.com>
2009-03-05 15:49:13 -0500

    New theme: rainbow sand. WordPress only at the moment

---
+++ themes/rainbowsand-wp/header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+ <head>
+  <meta charset='UTF-8'>
+  <meta name='verify-v1' content='fBP5k+EA4BFJx2aaK3hiOymbZSUbEAkp8OcEdeK/Fk8='>
+  <meta name='keywords' content='stilist, ratafia, ratafia currant'>
+  <title><?php wp_title(' — ', true, 'right'); ?><?php bloginfo('name'); ?></title>
+  <link rel='icon' type='text/css'
+        href='http://static.ratafia.info/favicon.ico'>
+  <link rel='stylesheet' type='text/css' media='screen'
+        href="<?php bloginfo('stylesheet_url'); ?>">
+  <link rel='openid.server' href='http://www.myopenid.com/server'>
+  <link rel='openid.delegate' href='http://stilist.myopenid.com/'>
+  <link rel='openid2.local_id' href='http://stilist.myopenid.com'>
+  <link rel='openid2.provider' href='http://www.myopenid.com/server'>
+  <meta http-equiv='X-XRDS-Location'
+        content='http://www.myopenid.com/xrds?username=stilist.myopenid.com'>
+  <link rel='alternate' type='application/atom+xml' title='Ratafia Currant'
+        href='http://feeds.feedburner.com/ratafia'>
+  <?php if ( is_singular() ) { ?><link rel='canonical'
+        href='<?php the_permalink(); ?>'><?php } ?>
+ </head>
+ <body>
+  <div id='wrapper'>
+   <ul id='nameplate'>
+    <li id='logo'><a href='<?php echo get_option('home'); ?>/' rel='home'><?php bloginfo('name'); ?></a></li>
+    <li><a href='/archive.php' id='archives'>Archives</a></li>
+    <li><a href='http://delicious.com/stilist' id='links'>Links</a></li>
+   </ul>
+
+   <div id='search'>
+    <?php get_search_form(); ?>
+   </div>
---
+++ themes/rainbowsand-wp/single.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+
+  <div id='content'>
+   <div class='entry'>
+   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <h1><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h1>
+    <div class='copy'>
+     <?php the_content(); ?>
+    </div>
+
+    <p class='signoff'>written <a href='<?php the_time('0Y\/F\/j'); ?>'><?php the_time('j F, 0Y'); ?></a>
+     • <a href='<?php the_permalink(); ?>#disqus_thread'>Comments</a>
+     <a href='<?php the_permalink(); ?>' class='pilcrow' title='Permanent link to this entry'>⁋</a></p>
+
+    <!-- disqus -->
+    <script>
+     var disqus_url = '<?php the_permalink() ?>';
+     var disqus_title = '<?php the_title(); ?>';
+    </script>
+    <div id='disqus_thread'></div>
+    <script src='http://disqus.com/forums/ratafia/embed.js'></script>
+    <script>
+     //<![CDATA[
+     (function() {
+      var links = document.getElementsByTagName('a');
+      var query = '?';
+      for(var i = 0; i < links.length; i++) {
+       if(links[i].href.indexOf('#disqus_thread') >= 0) {
+        query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
+       }
+      }
+      document.write('<script src="http://disqus.com/forums/ratafia/get_num_replies.js' + query + '"></' + 'script>');
+     })();
+     //]]>
+    </script>
+    <!-- /disqus -->
+   <?php endwhile; else: ?>
+    <h1>Bzzzt!</h1>
+    <p>Whatever you were looking for, this isn’t it.</p>
+   <?php endif; ?>
+   </div>
+  </div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
